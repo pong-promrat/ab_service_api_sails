dbeb0f01c834f2e8477b8dcfa95d1c5ca184c187
Johnny Hausman <johnny@hausmanfamily.com>
2019-11-13 22:25:37 +0700

    [add] eslint changes

--- api/controllers/file_processor/create.js
+++ api/controllers/file_processor/create.js
@@ -16,15 +16,15 @@
 const async = require("async");
 const cote = require("cote");
 const client = new cote.Requester({
-  name: "api_sails > file_processor > create"
+    name: "api_sails > file_processor > create"
 });
 const path = require("path");
 const shell = require("shelljs");
 const shortid = require("shortid");
 // setup our base path:
 var pathFiles =
-  sails.config.file_processor.basePath ||
-  path.sep + path.join("data", "file_processor");
+    sails.config.file_processor.basePath ||
+    path.sep + path.join("data", "file_processor");
 
 // create that path if it doesn't already exist:
 shell.mkdir("-p", pathFiles);
@@ -37,136 +37,142 @@ var requiredParams = ["appKey", "permission"];
 
 // make sure our BasePath is created:
 module.exports = function(req, res) {
-  // Package the Find Request and pass it off to the service
+    // Package the Find Request and pass it off to the service
 
-  var jobID = shortid.generate();
+    var jobID = shortid.generate();
 
-  sails.log(`file_processor::create::${jobID}`);
+    sails.log(`file_processor::create::${jobID}`);
 
-  // create a new job for the file_processor
-  let jobData = {
-    jobID: jobID
-  };
+    // create a new job for the file_processor
+    let jobData = {
+        jobID: jobID
+    };
 
-  // params
-  var options = {};
+    // params
+    var options = {};
 
-  var serviceResponse;
+    var serviceResponse;
 
-  async.series(
-    [
-      // 1) finish downloading the file
-      next => {
-        req.file("file").upload(
-          {
-            // store the files in our TEMP path
-            dirname: path.join(
-              pathFiles,
-              sails.config.file_processor.uploadPath || "tmp"
-            ),
-            maxBytes: sails.config.file_processor.maxBytes || 10000000
-          },
-          function(err, list) {
-            if (err) {
-              err.code = 500;
-              next(err);
-            } else {
-              fileEntry = list[0]; // info about file
-              console.log("... fileEntry:", fileEntry);
+    async.series(
+        [
+            // 1) finish downloading the file
+            (next) => {
+                req.file("file").upload(
+                    {
+                        // store the files in our TEMP path
+                        dirname: path.join(
+                            pathFiles,
+                            sails.config.file_processor.uploadPath || "tmp"
+                        ),
+                        maxBytes:
+                            sails.config.file_processor.maxBytes || 10000000
+                    },
+                    function(err, list) {
+                        if (err) {
+                            err.code = 500;
+                            next(err);
+                        } else {
+                            fileEntry = list[0]; // info about file
+                            console.log("... fileEntry:", fileEntry);
 
-              if (fileEntry) {
-                fileRef = fileEntry.fd; // full path to file
+                            if (fileEntry) {
+                                fileRef = fileEntry.fd; // full path to file
+                                next();
+                            } else {
+                                var err = new Error(
+                                    "No file uploaded for parameter [file]"
+                                );
+                                err.code = 422;
+                                next(err);
+                            }
+                        }
+                    }
+                );
+            },
+
+            // 2) read in the parameters
+            (next) => {
+                params.forEach(function(p) {
+                    options[p] = req.param(p) || "??";
+                });
+
+                var missingParams = [];
+                requiredParams.forEach(function(r) {
+                    if (options[r] == "??") {
+                        missingParams.push(r);
+                    }
+                });
+
+                if (missingParams.length > 0) {
+                    console.log("... missingParams:", missingParams);
+                    // var error = ADCore.error.fromKey('E_MISSINGPARAM');
+                    var error = new Error("Missing Parameter");
+                    error.key = "E_MISSINGPARAM";
+                    error.missingParams = missingParams;
+                    error.code = 422;
+                    next(error);
+                    return;
+                }
+
                 next();
-              } else {
-                var err = new Error("No file uploaded for parameter [file]");
-                err.code = 422;
-                next(err);
-              }
+            },
+
+            // 3) pass the job to the client
+            (next) => {
+                jobData.name = fileEntry.fd.split(path.sep).pop();
+                jobData.appKey = options.appKey;
+                jobData.permission = options.permission;
+                jobData.size = fileEntry.size;
+                jobData.type = fileEntry.type;
+                jobData.fileName = fileEntry.filename;
+
+                // TODO: for now, reuse the user.id values so we can test with
+                // existing system DB values.  When we switch to multi tenant
+                // then use the user.uuid
+                jobData.userUUID = 1; //"??"; // TODO: after we have user info (user.uuid)
+                jobData.tenant = "__"; // TODO: after we have Tenant Info
+
+                // pass the request off to the uService:
+                client.send(
+                    { type: "file.upload", param: jobData },
+                    (err, results) => {
+                        serviceResponse = results;
+                        next(err);
+                    }
+                );
+            },
+
+            // 3) package response to the client
+            (next) => {
+                sails.log(serviceResponse);
+
+                // TODO: verify serviceResponse has uuid
+
+                var data = {
+                    uuid: serviceResponse.uuid
+                };
+
+                // if this was a Webix uploader:
+                if (
+                    options.isWebix != "??" &&
+                    options.isWebix != "false" &&
+                    options.isWebix != false
+                ) {
+                    data.status = "server";
+                }
+
+                res.json(data);
+                next();
             }
-          }
-        );
-      },
-
-      // 2) read in the parameters
-      next => {
-        params.forEach(function(p) {
-          options[p] = req.param(p) || "??";
-        });
-
-        var missingParams = [];
-        requiredParams.forEach(function(r) {
-          if (options[r] == "??") {
-            missingParams.push(r);
-          }
-        });
-
-        if (missingParams.length > 0) {
-          console.log("... missingParams:", missingParams);
-          // var error = ADCore.error.fromKey('E_MISSINGPARAM');
-          var error = new Error("Missing Parameter");
-          error.key = "E_MISSINGPARAM";
-          error.missingParams = missingParams;
-          error.code = 422;
-          next(error);
-          return;
+        ],
+        (err, results) => {
+            // handle error reporting back to the client
+            if (err) {
+                sails.log(results);
+                res.json(results);
+            }
         }
-
-        next();
-      },
-
-      // 3) pass the job to the client
-      next => {
-        jobData.name = fileEntry.fd.split(path.sep).pop();
-        jobData.appKey = options.appKey;
-        jobData.permission = options.permission;
-        jobData.size = fileEntry.size;
-        jobData.type = fileEntry.type;
-        jobData.fileName = fileEntry.filename;
-
-        // TODO: for now, reuse the user.id values so we can test with
-        // existing system DB values.  When we switch to multi tenant
-        // then use the user.uuid
-        jobData.userUUID = 1; //"??"; // TODO: after we have user info (user.uuid)
-        jobData.tenant = "__"; // TODO: after we have Tenant Info
-
-        // pass the request off to the uService:
-        client.send({ type: "file.upload", param: jobData }, (err, results) => {
-          serviceResponse = results;
-          next(err);
-        });
-      },
-
-      // 3) package response to the client
-      next => {
-        sails.log(serviceResponse);
-
-        // TODO: verify serviceResponse has uuid
-
-        var data = {
-          uuid: serviceResponse.uuid
-        };
-
-        // if this was a Webix uploader:
-        if (
-          options.isWebix != "??" &&
-          options.isWebix != "false" &&
-          options.isWebix != false
-        ) {
-          data.status = "server";
-        }
-
-        res.json(data);
-        next();
-      }
-    ],
-    (err, results) => {
-      // handle error reporting back to the client
-      if (err) {
-        sails.log(results);
-        res.json(results);
-      }
-    }
-  );
+    );
 };
 
 // TODO: testing:
--- api/controllers/model/find.js
+++ api/controllers/model/find.js
@@ -9,23 +9,23 @@ const cote = require("cote");
 const client = new cote.Requester({ name: "ModelFindRequestor" });
 
 module.exports = function(req, res) {
-  // Package the Find Request and pass it off to the service
+    // Package the Find Request and pass it off to the service
 
-  sails.log("Model.Find()");
+    sails.log("Model.Find()");
 
-  // gather .find() parameters here
-  let findData = {
-    user: "userInfo",
-    tenant: "tenantID",
-    where: { id: 1 },
-    sort: "asc",
-    start: 0,
-    limit: 20
-  };
+    // gather .find() parameters here
+    let findData = {
+        user: "userInfo",
+        tenant: "tenantID",
+        where: { id: 1 },
+        sort: "asc",
+        start: 0,
+        limit: 20
+    };
 
-  // pass the request off to the uService:
-  client.send({ type: "model.find", param: findData }, results => {
-    sails.log(results);
-    res.json(results);
-  });
+    // pass the request off to the uService:
+    client.send({ type: "model.find", param: findData }, (results) => {
+        sails.log(results);
+        res.json(results);
+    });
 };
