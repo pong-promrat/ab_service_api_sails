6adfc8a890e6b957448705ef6bea58956b4e2c92
Johnny Hausman <johnny@hausmanfamily.com>
2021-08-23 14:11:45 +0700

    [add] include an import route for use in writing our e2e tests.

---
+++ api/controllers/definition_manager/test-import.js
@@ -0,0 +1,85 @@
+/**
+ * definition_manager/test-import.js
+ *
+ *
+ * url:     post /test/import
+ * header:  X-CSRF-Token : [token]
+ * params:
+ */
+var fs = require("fs");
+var path = require("path");
+
+var inputParams = {
+   file: { string: true, required: true },
+};
+
+// make sure our BasePath is created:
+module.exports = function (req, res) {
+   // Package the Find Request and pass it off to the service
+
+   req.ab.log(`definition_manager::test-import`);
+
+   // if this isn't in the testing environment, 404!
+   if (typeof process.env.AB_TESTING == "undefined") {
+      return res.notFound();
+   }
+
+   // verify your inputs are correct:
+   // false : prevents an auto error response if detected. (default: true)
+   // valuesToCheck: {obj} a specified subset of the input values to validate.
+   //    { [key] : [value] }
+   //       [key] = inputParams[key] entry .
+   //       [value] = req.param(value)
+   //    if no values given, then req.allParams() are evaluated. In some cases
+   //    you'll want to only require a certain subset of input values and then
+   //    let the rest be evaluated by the destination service.
+   if (
+      !(req.ab.validUser(/* false */)) ||
+      !req.ab.validateParameters(inputParams /*, false , valuesToCheck*/)
+   ) {
+      // an error message is automatically returned to the client
+      // so be sure to return here;
+      return;
+   }
+
+   var file = req.ab.param("file");
+
+   fs.readFile(path.join(process.cwd(), file), function (err, data) {
+      if (err) {
+         req.ab.notify.developer(err, {
+            context: "api_sails::definition_manager.test-import.js",
+            message: "error reading specified file.",
+            file,
+         });
+         res.send({ status: "error" });
+         //res.AD.error(err);
+      } else {
+         try {
+            var json = JSON.parse(data.toString());
+
+            // create a new job for the service
+            let jobData = {
+               json,
+            };
+
+            // pass the request off to the uService:
+            req.ab.serviceRequest(
+               "definition_manager.json-import",
+               jobData,
+               (err /*, results */) => {
+                  if (err) {
+                     req.ab.log("Error in json-import : ", err);
+                     res.ab.error(err);
+                     return;
+                  }
+                  res.ab.success({ done: true });
+               }
+            );
+         } catch (err) {
+            req.ab.log("json-import parse error", err);
+            err.message = `json parse error : ${err.message}`;
+            res.ab.error(err, 500);
+         }
+      }
+   });
+};
