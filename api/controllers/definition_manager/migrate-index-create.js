bd2e5fd1f2e51a99783c3ca41f53244f7a9095e6
Johnny Hausman <johnny@hausmanfamily.com>
2022-01-28 17:26:18 +0700

    [add] migrate Index api

---
+++ api/controllers/definition_manager/migrate-index-create.js
@@ -0,0 +1,48 @@
+/**
+ * definition_manager/migrate-index-create.js
+ *
+ *
+ * url:     post /definition/migrate/object/:objID/index/:ID
+ * header:  X-CSRF-Token : [token]
+ * params:
+ */
+
+var inputParams = {
+   objID: { string: { uuid: true }, required: true },
+   ID: { string: { uuid: true }, required: true },
+};
+
+// make sure our BasePath is created:
+module.exports = function (req, res) {
+   req.ab.log(`definition_manager::migrate-index-create`);
+
+   // verify your inputs are correct:
+   if (
+      !(req.ab.validUser(/* false */)) ||
+      !(req.ab.validBuilder(/* false */)) ||
+      !req.ab.validateParameters(inputParams /*, false , valuesToCheck*/)
+   ) {
+      // an error message is automatically returned to the client
+      // so be sure to return here;
+      return;
+   }
+
+   // create a new job for the service
+   let jobData = {
+      objID: req.ab.param("objID"),
+      ID: req.ab.param("ID"),
+   };
+
+   // pass the request off to the uService:
+   req.ab.serviceRequest(
+      "definition_manager.migrate-index-create",
+      jobData,
+      (err, results) => {
+         if (err) {
+            res.ab.error(err);
+            return;
+         }
+         res.ab.success(results);
+      }
+   );
+};
