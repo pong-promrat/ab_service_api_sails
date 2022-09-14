0a29247ce0c0329f3684d7d5c89a7d5a56e6bf20
nohrsom <sky_wyvern@hotmail.com>
2022-09-14 10:21:28 +0700

    Refactor multi-tenant update

--- api/controllers/definition_manager/tenants-update-application.js
+++ api/controllers/definition_manager/tenants-update-application.js
@@ -6,8 +6,18 @@
  * header:  X-CSRF-Token : [token]
  * params:
  */
+
+const path = require("path");
+const fs = require("fs");
+
 const inputParams = {
-   data: { object: true, required: true },
+   applicationUUID: { string: true, required: true },
+   // Use for split a definition json file in coming.
+   // States { 0: create the file, 1: tranfer data of those keys, 2: done }.
+   state: { number: { integer: true }, require: true },
+   // Number of milli seconds
+   date: { date: true, required: true },
+   data: { string: true, optional: true },
    /*    "email": { string:{ email: { allowUnicode: true }}, required:true }   */
    /*                -> NOTE: put .string  before .required                    */
    /*    "param": { required: true } // NOTE: param Joi.any().required();      */
@@ -36,31 +46,80 @@ module.exports = function (req, res) {
       return;
    }
 
+   const applicationUUID = req.ab.param("applicationUUID");
+   const state = req.ab.param("state");
+   const date = req.ab.param("date");
    const data = req.ab.param("data");
 
+   const pathStorage = path.join(
+      sails.config.paths.tmp,
+      "tenants_update_application"
+   );
+
+   if (!fs.existsSync(pathStorage)) {
+      fs.mkdirSync(pathStorage);
+   }
+
+   const filename = path.join(
+      pathStorage,
+      `${date.toISOString()}_${applicationUUID}.json`
+   );
+
    try {
-      // create a new job for the service
-      const jobData = {
-         data,
-      };
+      switch (state) {
+         case 0:
+            fs.writeFileSync(filename, data || "");
 
-      // pass the request off to the uService:
-      req.ab.serviceRequest(
-         "definition_manager.tenants-update-application",
-         jobData,
-         (error /*, results */) => {
-            if (error) {
-               req.ab.log("Error in tenants-update-application : ", error);
-               res.ab.error(error);
+            res.ab.success({});
 
-               return;
-            }
-            res.ab.success({ done: true });
-         }
-      );
+            break;
+
+         case 1:
+            if (!fs.existsSync(filename))
+               throw Error(
+                  'You need to the parameter "state: 0" before this state'
+               );
+
+            if (!data)
+               throw Error(
+                  'You need to send the parameters "data" on this state'
+               );
+            fs.appendFileSync(filename, data);
+            res.ab.success({});
+
+            break;
+
+         case 2:
+            // pass the request off to the uService:
+            req.ab.serviceRequest(
+               "definition_manager.tenants-update-application",
+               // the parameter "jobData"
+               {
+                  applicationUUID,
+                  data: require(filename),
+                  longRequest: true, // Tell cote to wait longer as import takes time.
+               },
+               (error /*, results */) => {
+                  if (error) {
+                     req.ab.log("Error in tenants-update-application: ", error);
+                     res.ab.error(error);
+
+                     return;
+                  }
+                  res.ab.success({});
+               }
+            );
+
+            break;
+
+         default:
+            throw Error(`We don't have the specific state ${state}`);
+      }
    } catch (error) {
+      if (fs.existsSync(filename)) fs.unlinkSync(filename);
+
       req.ab.log("tenants-update-application parse error", error);
-      error.message = `json parse error : ${error.message}`;
+      error.message = `Tranfer files error: ${error.message}`;
       res.ab.error(error, 500);
    }
 };
