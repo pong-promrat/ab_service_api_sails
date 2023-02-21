8499e399231576a34944b91feee6d3da6efb0bee
Johnny Hausman <johnny@hausmanfamily.com>
2023-02-21 17:44:42 -0600

    [add] included new api endpoints for the ABProcessTriggerTimer implementation.

---
+++ api/controllers/process_manager/timer-status.js
@@ -0,0 +1,44 @@
+/**
+ * process_manager/timer-status.js
+ *
+ *
+ * url:     get /process/timer/:ID
+ * header:  X-CSRF-Token : [token]
+ * params:
+ */
+
+var inputParams = {
+   ID: { string: { uuid: true }, required: true },
+};
+
+// make sure our BasePath is created:
+module.exports = function (req, res) {
+   // Package the Find Request and pass it off to the service
+
+   req.ab.log(`process_manager::timer-status`);
+
+   // verify your inputs are correct:
+   if (
+      !(req.ab.validUser(/* false */)) ||
+      !(req.ab.validBuilder(/* false */)) ||
+      !req.ab.validateParameters(inputParams /*, false , valuesToCheck*/)
+   ) {
+      // an error message is automatically returned to the client
+      // so be sure to return here;
+      return;
+   }
+
+   // create a new job for the service
+   let jobData = {
+      uuid: req.ab.param("ID"),
+   };
+
+   // pass the request off to the uService:
+   req.ab.serviceRequest("process_manager.timer-status", jobData, (err, results) => {
+      if (err) {
+         res.ab.error(err);
+         return;
+      }
+      res.ab.success(results);
+   });
+};
