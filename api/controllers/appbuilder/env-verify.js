1dac7e89e7c33f165bb3899369fcbbb238a1136a
Johnny <johnny@digiserve.org>
2024-09-10 14:25:00 -0500

    [wip] env and metadata routes for Netsuite Object Definitions

---
+++ api/controllers/appbuilder/env-verify.js
@@ -0,0 +1,46 @@
+/**
+ * appbuilder/env-verify.js
+ *
+ *
+ * url:     get /env/verify/:ID
+ * header:  X-CSRF-Token : [token]
+ * params:
+ */
+
+var inputParams = {
+   ID: { string: true, required: true },
+};
+
+// make sure our BasePath is created:
+module.exports = function (req, res) {
+   // Package the Find Request and pass it off to the service
+
+   req.ab.log(`appbuilder::env-verify`);
+
+   // verify your inputs are correct:
+   // false : prevents an auto error response if detected. (default: true)
+   // valuesToCheck: {obj} a specified subset of the input values to validate.
+   if (
+      !(req.ab.validUser(/* false */)) ||
+      !(req.ab.validBuilder(/* false */)) ||
+      !req.ab.validateParameters(inputParams /*, false , valuesToCheck*/)
+   ) {
+      // an error message is automatically returned to the client
+      // so be sure to return here;
+      return;
+   }
+
+   // create a new job for the service
+   let jobData = {
+      id: req.ab.param("ID"),
+   };
+
+   // pass the request off to the uService:
+   req.ab.serviceRequest("appbuilder.env-verify", jobData, (err, results) => {
+      if (err) {
+         res.ab.error(err);
+         return;
+      }
+      res.ab.success(results);
+   });
+};
--- config/routes.js
+++ config/routes.js
@@ -186,4 +186,8 @@ module.exports.routes = {
 
    // Metrics
    "get /metrics": "metric_manager/metrics-information",
+
+   // Netsuite related
+   "get /env/verify/:ID": "appbuilder/env-verify",
+   "get /netsuite/metadata": "appbuilder/netsuite-metadata-catalog",
 };
